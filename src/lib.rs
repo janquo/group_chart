01fcaf8c1134cb7a8a279a974062cb469ee8eecc
Jan Guzik <jan.guzik@cs.ox.ac.uk>
2019-01-06 04:03:20 +0100

    closes #2

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,29 @@
+#![feature(try_trait)]
+
 //#[macro_use]
-extern crate serde_derive;
+extern crate image;
 extern crate reqwest;
 extern crate serde;
+extern crate serde_derive;
 extern crate serde_json;
 
 pub mod reader {
     use std::fs;
 
     pub fn get_users() -> String {
-        let contents = fs::read_to_string("users.txt").expect("Something went wrong reading the file");
+        let contents = fs::read_to_string("users.txt")
+            .expect("Something went wrong reading the users.txt file");
         contents
     }
     pub fn get_key() -> String {
-        let contents = fs::read_to_string("key.txt").expect("Something went wrong reading the file");
+        let contents =
+            fs::read_to_string("key.txt").expect("Something went wrong reading the key.txt file");
         contents
     }
 }
 pub mod communicator {
     use serde_json::Value;
+    use std::fs::File;
 
     pub fn get_chart(user: &str, key: &str) -> Result<Value, reqwest::Error> {
         let request_url = format!("http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyAlbumChart&user={}&api_key={}&format=json",
@@ -29,12 +35,74 @@ pub mod communicator {
         Ok(answer)
     }
 
-    pub fn parse_album(data: &Value) -> (String, i64) {
+    pub fn parse_album(data: &Value) -> (String, String, i64) {
         (
             String::from(data["name"].as_str().unwrap()),
+            String::from(data["artist"]["#text"].as_str().unwrap()),
             data["playcount"].as_str().unwrap().parse().unwrap(),
         )
     }
+
+    pub fn get_album_inf(name: &str, artist: &str, key: &str) -> Result<Value, reqwest::Error> {
+        let request_url = format!("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key={}&artist={}&album={}&format=json",
+                              key, artist, name);
+        let mut response = reqwest::get(&request_url)?;
+
+        let answer: Value = response.json()?;
+
+        Ok(answer)
+    }
+
+    pub fn get_img_url(data: &Value) -> Result<String, std::option::NoneError> {
+            Ok(String::from(data["album"]["image"].as_array()?[1]["#text"].as_str()?))
+    }
+
+    pub fn download_image(target: &str) -> Result<String, reqwest::Error> {
+        let mut response = reqwest::get(target)?;
+        let mut result = String::new();
+
+        let mut dest = {
+            let fname = response
+                .url()
+                .path_segments()
+                .and_then(|segments| segments.last())
+                .and_then(|name| if name.is_empty() { None } else { Some(name) })
+                .unwrap_or("tmp.bin");
+
+            result = String::from(fname);
+            File::create(fname).unwrap()
+        };
+        std::io::copy(&mut response, &mut dest).unwrap();
+        Ok(result)
+    }
 }
 
-pub mod drawer {}
+pub mod drawer {
+
+    pub fn play() {
+        use image::GenericImage;
+
+        let mut img = image::ImageBuffer::new(128, 64);
+
+        let img2 = image::open("img.png").unwrap();
+
+        img.copy_from(&img2, 0, 0);
+        img.copy_from(&img2, 64, 0);
+
+        img.save("test.png").unwrap();
+    }
+
+    pub fn collage(images: Vec<String>) {
+        use image::GenericImage;
+
+        let mut img = image::ImageBuffer::new(320, 320);
+
+        for i in 0..25 as usize {
+            let img2 = image::open(images[i].clone()).unwrap();
+
+            img.copy_from(&img2, 64*(i % 5) as u32, 64*(i / 5) as u32);
+        }
+
+        img.save("test.png").unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,64 @@
-use std::collections::HashMap;
-use group_chart::communicator;
+
+
+use group_chart::communicator::*;
 use group_chart::drawer;
 use group_chart::reader;
+use std::collections::HashMap;
 
 fn main() {
+    let top_number = 25;
+
     let users = reader::get_users();
-    let users : Vec<&str> = users.lines().collect();
+    let users: Vec<&str> = users.lines().collect();
 
     let key = reader::get_key();
 
-    let mut global_chart : HashMap<String, i64> = HashMap::new();
+    let mut global_chart: HashMap<(String, String), i64> = HashMap::new();
 
     for user in users {
-        let user_data = communicator::get_chart(user, &key);
+        let user_data = get_chart(user, &key);
 
         match user_data {
-            Err(x) => panic!(x),
+            Err(x) => {
+                eprintln!("Error reading user {}. Continue to next user. {}", user, x);
+                continue;
+            }
             _ => (),
         }
+
         let user_data = user_data.unwrap();
 
-        if !user_data["weeklyalbumchart"]["album"].is_array() {panic!("no array")};
+        if !user_data["weeklyalbumchart"]["album"].is_array() {
+            eprintln!("User {} has no scrobbles. Continue to next user.", user);
+            continue;
+        };
 
-        let user_albums : &Vec<serde_json::Value> = user_data["weeklyalbumchart"]["album"].as_array().unwrap();
-        for (name, count) in user_albums.iter().map(|x| communicator::parse_album(x)) {
-            let count_glob = global_chart.entry(name).or_insert(0);
+        let user_albums: &Vec<serde_json::Value> =
+            user_data["weeklyalbumchart"]["album"].as_array().unwrap();
+
+        for (name, artist, count) in user_albums.iter().map(|x| parse_album(x)) {
+            let count_glob = global_chart.entry((name, artist)).or_insert(0);
             *count_glob += count;
         }
     }
 
-    let mut global_chart_vec : Vec<(String, i64)> = Vec::new();
+    let mut global_chart_vec: Vec<((String, String), i64)> = Vec::new();
+
     for album in global_chart {
         global_chart_vec.push(album);
     }
 
     global_chart_vec.sort_by_key(|(_, count)| -(*count));
-    global_chart_vec.truncate(25);
-    for album in global_chart_vec {
-        println!("{} {}", album.0, album.1);
-    }
+    global_chart_vec.truncate(top_number);
 
+    let mut cover_urls : Vec<String> = Vec::new();
+    for album in global_chart_vec {
+        println!("{} - {}: {}", (album.0).1, (album.0).0, album.1);
+        let data = get_album_inf(&(album.0).0, &(album.0).1, &key).unwrap();
+        match get_img_url(&data) {
+            Ok(x) => cover_urls.push(download_image(&x).unwrap_or(String::from("blank.png"))),
+            _ => cover_urls.push(String::from("blank.png")),
+        }
+    }
+    drawer::collage(cover_urls);
 }
