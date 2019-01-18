cf41195a92ceaba75b4fd748c58b88b50d9f4dec
Jan Guzik <jan.guzik@cs.ox.ac.uk>
2019-01-18 20:01:21 +0000

    reformattting - struct Album and getting rid of modules #1 #4

--- src/lib.rs
+++ src/lib.rs
@@ -9,86 +9,152 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
-pub mod reader {
-    use std::fs;
+use serde_json::Value;
+use std::cmp::Ordering;
+use std::fs;
+use std::fs::File;
 
-    pub fn get_users() -> String {
-        let contents = fs::read_to_string("users.txt")
-            .expect("Something went wrong reading the users.txt file");
-        contents
-    }
-    pub fn get_key() -> String {
-        let contents =
-            fs::read_to_string("key.txt").expect("Something went wrong reading the key.txt file");
-        contents
-    }
+pub struct Album {
+    title: String,
+    artist: String,
+    playcount: i64,
+    tracks: Option<usize>,
+    score: Option<f64>,
+    pub image: Option<String>,
 }
-pub mod communicator {
-    use serde_json::Value;
-    use std::fs::File;
 
-    pub fn get_chart(user: &str, key: &str) -> Result<Value, reqwest::Error> {
-        let request_url = format!("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user={}&api_key={}&period=7day&format=json",
-                              user, key);
-        let mut response = reqwest::get(&request_url)?;
-
-        let answer: Value = response.json()?;
-
-        Ok(answer)
-    }
-
-    pub fn parse_album(data: &Value) -> (String, String, i64) {
-        (
-            String::from(data["name"].as_str().unwrap()),
-            String::from(data["artist"]["name"].as_str().unwrap()),
-            data["playcount"].as_str().unwrap().parse().unwrap(),
-        )
-    }
-
-    pub fn get_album_inf(name: &str, artist: &str, key: &str) -> Result<Value, reqwest::Error> {
-        let request_url = format!("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key={}&artist={}&album={}&format=json",
-                              key, artist, name);
-        let mut response = reqwest::get(&request_url)?;
-
-        let answer: Value = response.json()?;
-
-        Ok(answer)
-    }
-
-    pub fn get_img_url(data: &Value) -> Result<String, std::option::NoneError> {
-        Ok(String::from(
-            data["album"]["image"].as_array()?[1]["#text"].as_str()?,
-        ))
-    }
-
-    pub fn get_album_tracks_no(data: &Value) -> usize {
-        //eprintln!("{:?}", data["album"]["tracks"]);
-        let tracks = data["album"]["tracks"]["track"].as_array();
-        match tracks {
-            None => 15,
-            Some(x) => x.len(),
+impl Album {
+    pub fn parse_album(data: &Value) -> Album {
+        Album {
+            title: String::from(data["name"].as_str().unwrap()),
+            artist: String::from(data["artist"]["name"].as_str().unwrap()),
+            playcount: data["playcount"].as_str().unwrap().parse().unwrap(),
+            tracks: None,
+            score: None,
+            image: None,
         }
     }
 
-    pub fn download_image(target: &str) -> Result<String, reqwest::Error> {
-        let mut response = reqwest::get(target)?;
-        let mut result = String::new();
+    pub fn more_info(&mut self, key: &str) -> Result<(), std::option::NoneError> {
+        let request_url = format!("http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key={}&artist={}&album={}&format=json",
+                              key, self.artist, self.title);
+        let mut response = reqwest::get(&request_url).expect("no response from api");
 
-        let mut dest = {
-            let fname = response
-                .url()
-                .path_segments()
-                .and_then(|segments| segments.last())
-                .and_then(|name| if name.is_empty() { None } else { Some(name) })
-                .unwrap_or("tmp.bin");
-            let fname = format!("./images/{}", fname);
-            let fname = fname.as_str();
-            result = String::from(fname);
-            File::create(fname).unwrap()
+        let data = response.json();
+        let data : Value = match data {
+            Ok(x) => x,
+            _ => return Ok(()),
         };
-        std::io::copy(&mut response, &mut dest).unwrap();
-        Ok(result)
+
+        self.tracks = data["album"]["tracks"]["track"].as_array().map(|x| x.len());
+        if self.tracks == Some(0) {
+            self.tracks = None;
+        }
+
+        self.image = data["album"]["image"].as_array()?[2]["#text"]
+            .as_str()
+            .map(|s| String::from(s));
+
+        self.compute_score();
+
+        Ok(())
     }
+
+    fn compute_score(&mut self) {
+        if self.tracks.is_none() {
+            return;
+        }
+        self.score = Some(self.playcount as f64 / (self.tracks.unwrap() as f64));
+    }
+
+    pub fn incr_playcount(&mut self, other: &Album) {
+        self.playcount += other.playcount;
+    }
+
+    pub fn get_count(&self) -> i64 {
+        self.playcount
+    }
+
+    pub fn get_score(&self) -> Option<f64> {
+        self.score
+    }
+
+    pub fn compare_decr(&self, other: &Album) -> Ordering {
+        if self.score.is_none() || other.score.is_none() {
+            return other.playcount.cmp(&self.playcount);
+        }
+        other
+            .score
+            .unwrap()
+            .partial_cmp(&self.score.unwrap())
+            .unwrap()
+    }
+}
+impl PartialEq for Album {
+    fn eq(&self, other: &Album) -> bool {
+        self.title == other.title && self.artist == other.artist
+    }
+}
+impl Eq for Album {}
+impl Ord for Album {
+    fn cmp(&self, other: &Album) -> Ordering {
+        self.title.cmp(&other.title)
+    }
+}
+impl PartialOrd for Album {
+    fn partial_cmp(&self, other: &Album) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl std::fmt::Display for Album {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{} - {}, with score: {} ({})",
+            self.artist, self.title, self.playcount, self.score.unwrap_or(0f64)
+        )
+    }
+}
+
+pub fn get_users() -> String {
+    let contents =
+        fs::read_to_string("users.txt").expect("Something went wrong reading the users.txt file");
+    contents
+}
+pub fn get_key() -> String {
+    let contents =
+        fs::read_to_string("key.txt").expect("Something went wrong reading the key.txt file");
+    contents
+}
+
+pub fn get_chart(user: &str, key: &str) -> Result<Value, reqwest::Error> {
+    let request_url = format!("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user={}&api_key={}&period=7day&format=json",
+                              user, key);
+    let mut response = reqwest::get(&request_url)?;
+
+    let answer: Value = response.json()?;
+
+    Ok(answer)
+}
+
+pub fn download_image(target: &str) -> Result<String, reqwest::Error> {
+    let mut response = reqwest::get(target)?;
+    let mut result;
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+        let fname = format!("./images/{}", fname);
+        let fname = fname.as_str();
+        result = String::from(fname);
+        File::create(fname).unwrap()
+    };
+    std::io::copy(&mut response, &mut dest).unwrap();
+    Ok(result)
 }
 
 pub mod drawer {
@@ -96,12 +162,12 @@ pub mod drawer {
     pub fn collage(images: Vec<String>) {
         use image::GenericImage;
 
-        let mut img = image::ImageBuffer::new(320, 320);
+        let mut img = image::ImageBuffer::new(870, 870);
 
         for i in 0..25 as usize {
             let img2 = image::open(images[i].clone()).unwrap();
 
-            img.copy_from(&img2, 64 * (i % 5) as u32, 64 * (i / 5) as u32);
+            img.copy_from(&img2, 174 * (i % 5) as u32, 174 * (i / 5) as u32);
         }
         //draw_description(&mut img);
         img.save("test.png").unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
-use group_chart::communicator::*;
-use group_chart::drawer;
-use group_chart::reader;
-use std::collections::HashMap;
+use group_chart::*;
+use std::collections::BTreeSet;
 use std::{thread, time};
 
 fn main() {
     let top_number = 25;
 
-    let users = reader::get_users();
+    let users = get_users();
     let users: Vec<&str> = users.lines().collect();
 
-    let key = reader::get_key();
+    let key = get_key();
 
-    let mut global_chart: HashMap<(String, String), i64> = HashMap::new();
+    let mut albums: BTreeSet<Album> = BTreeSet::new();
 
     let no_users = users.len();
-    let mut progress = 0;
 
-    for user in users {
+    for (progress, user) in users.iter().enumerate() {
         let user_data = get_chart(user, &key);
 
         match user_data {
@@ -38,55 +35,51 @@ fn main() {
         let user_albums: &Vec<serde_json::Value> =
             user_data["topalbums"]["album"].as_array().unwrap();
 
-        for (name, artist, count) in user_albums.iter().map(|x| parse_album(x)) {
+        for album in user_albums.iter().map(|x| Album::parse_album(x)) {
             //eprintln!("adding {} to counter of album {} by user {}", count, name, user);
-            let count_glob = global_chart.entry((name, artist)).or_insert(0);
-            *count_glob += count;
+            //insert returns false if same entry exists in a set
+            if albums.contains(&album) {
+                let mut old = albums.take(&album).unwrap();
+                old.incr_playcount(&album);
+                albums.insert(old);
+            } else {
+                albums.insert(album);
+            }
         }
-        let quarter_second = time::Duration::from_millis(250);
-        progress += 1;
-        println!("{}/{}", progress, no_users);
-        thread::sleep(quarter_second);
+
+        println!("{}/{}", progress+1, no_users);
+        thread::sleep(time::Duration::from_millis(125)); // don't overuse server
     }
 
-    let mut global_chart_vec: Vec<((String, String), i64)> = Vec::new();
+    let mut albums: Vec<Album> = albums.into_iter().collect();
 
-    for album in global_chart {
-        global_chart_vec.push(album);
-    }
-
-    global_chart_vec.sort_by_key(|(_, count)| -(*count));
-    //global_chart_vec.truncate(top_number);
+    albums.sort_by_key(|album| -album.get_count());
 
     let mut cover_urls: Vec<String> = Vec::new();
-    let mut min_so_far = 1000f64;
-    let mut data_vec = Vec::new();
-    for album in global_chart_vec.iter() {
-        let data = get_album_inf(&(album.0).0, &(album.0).1, &key).unwrap();
-        let tracks_no = get_album_tracks_no(&data);
-        if tracks_no == 0 {
-            eprintln!("zero tracków na {}", (album.0).0);
-            continue;
-        }
-        let count_waged = (album.1 as f64) / (tracks_no as f64);
-        if min_so_far > count_waged {
-            min_so_far = count_waged
-        } else if data_vec.len() >= top_number {
+    let mut min_so_far = 100000f64;
+    let mut top_albums = Vec::new();
+
+    for mut album in albums.into_iter() {
+        eprintln!("{}", top_albums.len());
+        album.more_info(&key);
+        if (album.get_count() as f64) < min_so_far  && top_albums.len() >= top_number {
             break;
         }
-        data_vec.push((data.clone(), count_waged));
+        match album.get_score() {
+            Some(score) => min_so_far = f64::min(min_so_far, score),
+            None => (),
+        }
+        top_albums.push(album);
     }
-    let mut chart_zipped: Vec<(&((String, String), i64), &(serde_json::Value, f64))> =
-        global_chart_vec.iter().zip(data_vec.iter()).collect();
 
-    chart_zipped.sort_by(|(_, (_, x)), (_, (_, y))| y.partial_cmp(x).unwrap());
-    chart_zipped.truncate(top_number);
+    top_albums.sort_by(Album::compare_decr);
+    top_albums.truncate(top_number);
 
-    for (album, (data, points)) in chart_zipped {
-        println!("{} - {}: {}", (album.0).1, (album.0).0, album.1);
+    for album in top_albums {
+        println!("{}", album);
 
-        match get_img_url(&data) {
-            Ok(x) => cover_urls.push(download_image(&x).unwrap_or(String::from("blank.png"))),
+        match album.image {
+            Some(x) => cover_urls.push(download_image(&x).unwrap_or(String::from("blank.png"))),
             _ => cover_urls.push(String::from("blank.png")),
         }
     }
